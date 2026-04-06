0b4c6ab30d508b7ceefb4773a14e4227e31f7c48
AidCheng <cn.aiden.cheng@gmail.com>
2026-04-06 17:53:02 +0100

    Impl: Constant Folder

--- src/comp0012/main/ConstantFolder.java
+++ src/comp0012/main/ConstantFolder.java
@@ -3,20 +3,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
 
-import org.apache.bcel.classfile.ClassParser;
-import org.apache.bcel.classfile.Code;
-import org.apache.bcel.classfile.JavaClass;
-import org.apache.bcel.classfile.Method;
-import org.apache.bcel.generic.ClassGen;
-import org.apache.bcel.generic.ConstantPoolGen;
-import org.apache.bcel.generic.InstructionHandle;
-import org.apache.bcel.generic.InstructionList;
-import org.apache.bcel.util.InstructionFinder;
-import org.apache.bcel.generic.MethodGen;
-import org.apache.bcel.generic.TargetLostException;
-
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.generic.*;
 
 
 public class ConstantFolder
@@ -44,13 +33,178 @@ public class ConstantFolder
 		ConstantPoolGen cpgen = cgen.getConstantPool();
 
 		// Implement your optimization here
-        
-		this.optimized = gen.getJavaClass();
+		Method[] methods = cgen.getMethods();
+		for (Method method: methods) {
+			if (method.isAbstract() || method.isNative()) {
+				continue;
+			}
+
+			MethodGen mg = new MethodGen(method, cgen.getClassName(), cpgen);
+			InstructionList il = mg.getInstructionList();
+
+			if (il == null) {
+				continue;
+			}
+
+			boolean changed = foldConstants(il, cpgen);
+
+			if (changed) {
+				il.setPositions(true);
+				mg.setMaxStack();
+				mg.setMaxLocals();
+				cgen.replaceMethod(method, mg.getMethod());
+			}
+		}
+
+		this.optimized = cgen.getJavaClass();
+	}
+
+
+
+	private boolean foldConstants(InstructionList il, ConstantPoolGen cpgen) {
+		boolean changed = false;
+		InstructionHandle current = il.getStart();
+
+		while (current != null) {
+			InstructionHandle h1 = current;
+			InstructionHandle h2 = h1.getNext();
+			if (h2 == null) break;
+
+			InstructionHandle h3 = h2.getNext();
+			if (h3 == null) break;
+
+			Number v1 = getNumericValue(h1, cpgen);
+			Number v2 = getNumericValue(h2, cpgen);
+
+			if (v1 != null && v2 != null) {
+				Number result = null;
+				result = foldDispatcher(v1, v2, h3);
+
+				if (result != null) {
+					try {
+						InstructionHandle newHandle = handleResult(result, il, h1, cpgen);
+
+						il.delete(h1, h3);
+						changed = true;
+						current = newHandle;
+						continue;
+					} catch (TargetLostException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			current = current.getNext();
+		}
+
+		return changed;
+	}
+	
+	private InstructionHandle handleResult(Number result, InstructionList il, InstructionHandle h1, ConstantPoolGen cpgen) {
+		InstructionHandle newHandle = null;	
+		if (result instanceof Integer){
+			newHandle = il.insert(h1, new LDC(cpgen.addInteger((Integer) result)));
+		} else if (result instanceof Float) {
+			newHandle = il.insert(h1, new LDC(cpgen.addFloat((Float) result)));
+		} else if (result instanceof Long) {
+			newHandle = il.insert(h1, new LDC2_W(cpgen.addLong((Long) result)));
+		} else if (result instanceof Double) {
+			newHandle = il.insert(h1, new LDC2_W(cpgen.addDouble((Double) result)));
+		}
+		return newHandle;
+	}
+
+	private Number getNumericValue(InstructionHandle handle, ConstantPoolGen cpgen) {
+		Instruction instruction = handle.getInstruction();
+		if (instruction instanceof ConstantPushInstruction) {
+			Number value = ((ConstantPushInstruction) instruction).getValue();
+			if (value instanceof Integer ||
+				value instanceof  Float  ||
+				value instanceof  Long   ||
+				value instanceof  Double) {
+				return value;
+			}
+		}
+
+		if (instruction instanceof LDC) {
+			Object value = ((LDC) instruction).getValue(cpgen);
+			if (value instanceof Integer || value instanceof  Float) {
+				return (Number) value;
+			}
+		}
+
+		if (instruction instanceof LDC2_W) {
+			Object value = ((LDC2_W) instruction).getValue(cpgen);
+			if (value instanceof Long || value instanceof Double) {
+				return (Number) value;
+			}
+		}
+
+		return null;
+	}
+
+	private Number foldDispatcher(Number v1, Number v2, InstructionHandle h3) {
+		if (v1 instanceof Integer && v2 instanceof Integer) {
+			return foldIntOperation(v1.intValue(), v2.intValue(), h3);
+		}
+
+		if (v1 instanceof Float && v2 instanceof Float) {
+			return foldFloatOperation(v1.floatValue(), v2.floatValue(), h3);
+		}
+
+		if (v1 instanceof Long && v2 instanceof Long) {
+			return foldLongOperation(v1.longValue(), v2.longValue(), h3);
+		}
+
+		if (v1 instanceof Double && v2 instanceof Double) {
+			return foldDoubleOperation(v1.doubleValue(), v2.doubleValue(), h3);
+		}
+
+		return null;
+	}
+
+	private Float foldFloatOperation(Float a, Float b, InstructionHandle handle) {
+		Instruction op = handle.getInstruction();
+		if (op instanceof FADD) return a + b;
+		if (op instanceof FSUB) return a - b;
+		if (op instanceof FMUL) return a * b;
+		if (op instanceof FDIV) return a / b;
+		return null;
+	}
+
+	private Long foldLongOperation(Long a, Long b, InstructionHandle handle) {
+		Instruction op = handle.getInstruction();
+		if (op instanceof LADD) return a + b;
+		if (op instanceof LSUB) return a - b;
+		if (op instanceof LMUL) return a * b;
+		if (op instanceof LDIV) {
+			if (b != 0) return a / b;
+		}
+		return null;
+	}
+
+	private Double foldDoubleOperation(Double a, Double b, InstructionHandle handle) {
+		Instruction op = handle.getInstruction();
+		if (op instanceof DADD) return a + b;
+		if (op instanceof DSUB) return a - b;
+		if (op instanceof DMUL) return a * b;
+		if (op instanceof DDIV) return a / b;
+		return null;
+	}
+
+
+	private Integer foldIntOperation(Integer a, Integer b, InstructionHandle handle){
+		Instruction op = handle.getInstruction();
+		if (op instanceof IADD) return a + b;
+		if (op instanceof ISUB) return a - b;
+		if (op instanceof IMUL) return a * b;
+		if (op instanceof IDIV) {
+			if (b != 0) return a / b;
+		}
+		return null;
 	}
 
 	
-	public void write(String optimisedFilePath)
-	{
+	public void write(String optimisedFilePath) {
 		this.optimize();
 
 		try {
